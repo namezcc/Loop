a3294ba997e7e5f50068c3c540c72870ccc6f9eb
namezcc <838865479@qq.com>
2021-03-23 14:22:36 +0800

    gitlab test

--- _out/test.bat
+++ _out/test.bat
@@ -1,10 +1,24 @@
 @echo off
 cd Debug
 
-start Server.exe -t 2 -n 1 -p 22001
-start Server.exe -t 3 -n 1 -p 23001
-start Server.exe -t 1 -n 1 -p 21001 
-start Server.exe -t 7 -n 1 -p 27001 
-start Server.exe -t 10 -n 1 -p 30001 
-start Server.exe -t 8 -n 1 -p 28001 
-start Server.exe -t 9 -n 1 -p 29001
+
+start Server.exe -t 2 -n 1 -p 1
+start Server.exe -t 1 -n 1 -p 1
+start Server.exe -t 1 -n 11 -p 1
+
+start Server.exe -t 7 -n 1 -p 1
+start Server.exe -t 7 -n 2 -p 1
+start Server.exe -t 7 -n 3 -p 1
+
+start Server.exe -t 3 -n 1 -p 1
+start Server.exe -t 3 -n 2 -p 1
+start Server.exe -t 3 -n 3 -p 1
+::--------------------------------------
+
+::start Server.exe -t 1 -n 11 -p 21011
+::start Server.exe -t 13 -n 1 -p 33001
+::start Server.exe -t 14 -n 1 -p 34001
+::start Server.exe -t 11 -n 1 -p 31001
+::start Server.exe -t 8 -n 1 -p 28001
+::start Server.exe -t 12 -n 1 -p 32001
+
